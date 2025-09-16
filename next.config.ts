40f67bfc97d71b47e3caa8f63166171186627acb
likanug <likanug.g@qq.com>
2025-09-16 14:19:20 +0800

    feat: 添加nextjs的图片加载白名单地址

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
-    /* config options here */
+    images: {
+        domains: [  // 允许加载该域名的图片
+            'img1.doubanio.com',
+            'img2.doubanio.com',
+            'img3.doubanio.com',
+            'img4.doubanio.com',
+            'img5.doubanio.com',
+            'img6.doubanio.com',
+            'img7.doubanio.com',
+            'img8.doubanio.com',
+            'img9.doubanio.com',
+        ],
+    },
 };
 
 export default nextConfig;
