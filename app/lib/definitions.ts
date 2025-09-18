8bfffb5670981079e8ae07f68a18a228debd248a
likanug <likanug.g@qq.com>
2025-09-18 13:51:27 +0800

    refactor: 删除无用的类型定义

--- app/lib/definitions.ts
+++ app/lib/definitions.ts
@@ -13,23 +13,6 @@ export type User = {
     updated_at: number;
 };
 
-export type Customer = {
-    id: string;
-    name: string;
-    email: string;
-    image_url: string;
-};
-
-export type Invoice = {
-    id: string;
-    customer_id: string;
-    amount: number;
-    date: string;
-    // In TypeScript, this is called a string union type.
-    // It means that the "status" property can only be one of the two strings: 'pending' or 'paid'.
-    status: 'pending' | 'paid';
-};
-
 export type Revenue = {
     month: string;
     revenue: number;
