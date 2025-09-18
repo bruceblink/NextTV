8a3c37f5a2fc4da744f3d31bb378fb9e36f1ea12
likanug <likanug.g@qq.com>
2025-09-18 13:38:32 +0800

    refactor: 重构spider_test.ts的实现，同时使用API和网页提取页面数据

--- app/lib/spider_test.ts
+++ app/lib/spider_test.ts
@@ -1,7 +1,18 @@
 import axios from "axios";
 import * as cheerio from "cheerio";
 
-const URL = "https://movie.douban.com/subject/36221247/";
+const DOUBAN_API_HEADER = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36',
+    'Origin': 'https://movie.douban.com',
+    'Referer': 'https://movie.douban.com/explore?',
+    'Accept': 'application/json, text/plain, */*',
+    'Cookie': 'll="108296"; bid=Yy4fcMVaTz0; _vwo_uuid_v2=DEDDB88D38ADE89FE8B880E5924C91791|5776df5806e256a0da98731dd0d2e045; __utma=30149280.1865394102.1758012698.1758012698.1758012698.1; __utmz=30149280.1758012698.1.1.utmcsr=movie.douban.com|utmccn=(referral)|utmcmd=referral|utmcct=/explore; _ga=GA1.1.1487530939.1758012790; _ga_RXNMP372GL=GS2.1.s1758012789$o1$g0$t1758012791$j58$l0$h0; dbcl2="223666985:5oShAVxYVAo"; push_noty_num=0; push_doumail_num=0; frodotk="01cc8c7aad01e62c0565a5700705b284"; talionusr="eyJpZCI6ICIyMjM2NjY5ODUiLCAibmFtZSI6ICJMIn0="; ck=sLqV; frodotk_db="4ceb998985e00eeee6c2cc9c91a02cca"'
+}
+
+const URL = "https://movie.douban.com/subject/36176467/";
+
+const URL_API = "https://m.douban.com/rexxar/api/v2/movie/36176467";
+
 
 /**
  * 获取豆瓣电影信息
@@ -9,16 +20,44 @@ const URL = "https://movie.douban.com/subject/36221247/";
 async function fetchDoubanVideoInfo(url: string): Promise<Record<string, any>> {
     try {
         const response = await axios.get(url, {
-            headers: {
-                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)",
-                Referer: "https://movie.douban.com/",
-            },
+            headers: DOUBAN_API_HEADER,
         });
         return parseDoubanMovieInfo(response.data);
     } catch (err) {
         console.error("抓取失败:", err);
         return {};
     }
+}
+
+async function fetchDoubanVideoInfoFromApi(url: string): Promise<Record<string, any>>{
+    try {
+        const response = await axios.get(url, {
+            headers: DOUBAN_API_HEADER,
+        });
+        const data = response.data;
+
+        const durations = data.durations as string[];
+        const durationsResult = durations.map(dura => {
+            const match = dura.match(/\d+/);
+            return match ? parseInt(match[0], 10) : null;
+        });
+
+        return {
+            original_title: data.original_title,
+            release_year: data.year,
+            intro: data.intro,
+            director: data.directors,
+            language: data.languages,
+            actors: data.actors,
+            duration: durationsResult,
+            production_country: data.countries,
+            aka: data.aka
+        }
+    } catch (err) {
+        console.error("抓取失败:", err);
+        return {};
+    }
+
 }
 
 /**
@@ -29,36 +68,25 @@ function parseDoubanMovieInfo(html: string): Record<string, any> {
 
     const movieInfo: Record<string, any> = {
         title: $("#content h1 span[property='v:itemreviewed']").text().trim(),
-        release_year: parseInt($("#content h1 span.year").text().replace(/[()]/g, "").trim()) || null,
     };
 
     // 英文 key 的提取器
     const extractors: Record<string, () => any> = {
-        director: () =>
-            $('a[rel="v:directedBy"]')
-                .map((_, el) => $(el).text().trim())
-                .get(),
+        // 编剧
         screenwriter: () =>
             $('#info span:contains("编剧") .attrs a')
                 .map((_, el) => $(el).text().trim())
                 .get(),
-        actors: () =>
-            $('a[rel="v:starring"]')
-                .map((_, el) => $(el).text().trim())
-                .get(),
-        type: () =>
+        // 类型
+        genres: () =>
             $('span[property="v:genre"]')
                 .map((_, el) => $(el).text().trim())
                 .get(),
+        // 上映日期
         release_date: () =>
             $('span[property="v:initialReleaseDate"]')
                 .map((_, el) => $(el).text().trim())
                 .get(),
-        duration: () => {
-            const runtime = $('span[property="v:runtime"]').text().trim();
-            const match = runtime.match(/\d+/); // 匹配第一个数字
-            return match ? parseInt(match[0], 10) : null;
-        },
     };
 
     // 基础字段（英文 key）
@@ -66,7 +94,7 @@ function parseDoubanMovieInfo(html: string): Record<string, any> {
         movieInfo[key] = extractors[key]();
     }
 
-    // info 区域其他字段（制片国家/地区、语言、IMDb 等）
+    // info 区域其他字段（IMDb）
     $("#info .pl").each((_, el) => {
         const label = $(el).text().trim().replace(/:$/, "");
         const textNode = el.next;
@@ -74,27 +102,23 @@ function parseDoubanMovieInfo(html: string): Record<string, any> {
             textNode && textNode.type === "text" ? textNode.data.trim() : "";
 
         switch (label) {
-            case "制片国家/地区":
-                movieInfo.production_country = value;
-                break;
-            case "语言":
-                movieInfo.language = value;
-                break;
             case "IMDb":
                 movieInfo.imdb = value;
                 break;
-            case "又名":
-                movieInfo.aka = value;
-                break;
             default:
-                // 其他暂时忽略或存储到 extra
-                if (!movieInfo.extra) movieInfo.extra = {};
-                movieInfo.extra[label] = value;
+               break;
         }
     });
 
     return movieInfo;
 }
 
-const res = await fetchDoubanVideoInfo(URL);
-console.log(res);
+const [res1, res2] = await Promise.all([
+    fetchDoubanVideoInfo(URL),
+    fetchDoubanVideoInfoFromApi(URL_API),
+
+]);
+
+const merged = { ...res1, ...res2 }; // 后者的同名字段会覆盖前者
+console.log(merged);
+
