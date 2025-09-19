ecc863b46f95a5016fb0d84d5adb987a277fb134
likanug <likanug.g@qq.com>
2025-09-19 18:53:32 +0800

    refactor: 将/app/lib/utils.ts中豆瓣相关的函数拆分到/app/lib/douban.ts中，并更新相关的package引用

--- app/lib/douban.ts
+++ app/lib/douban.ts
@@ -2,7 +2,8 @@
 // 配置超时的常量
 import {insertVideosToDB, ITEMS_PER_PAGE} from "@/app/lib/data";
 import {DOUBAN_API_HEADER, Video} from "@/app/lib/definitions";
-import {DoubanUrlUtils} from "@/app/lib/utils";
+import axios from "axios";
+import * as cheerio from "cheerio";
 
 const REQUEST_TIMEOUT = 10000; // 超时控制（10秒）
 
@@ -70,11 +71,122 @@ export async function fetchDoubanDataById(douban_id: string): Promise<any> {
     const timeoutId = setTimeout(() => controller.abort(), REQUEST_TIMEOUT);
 
     try {
-        const ut = new DoubanUrlUtils();
-        return await ut.fetchDoubanVideoInfoById(douban_id);
+        return await fetchDoubanVideoInfoById(douban_id);
     } catch (error) {
         clearTimeout(timeoutId); // 确保清除超时
         console.error("豆瓣 API 请求失败: ", error);
         return {};
     }
+}
+
+/**
+ * 获取豆瓣电影信息
+ */
+async function fetchDoubanVideoInfo(id: string): Promise<Record<string, any>> {
+    const URL =  (id: string) =>`https://movie.douban.com/subject/${id}`;
+    try {
+        const response = await axios.get(URL(id), {
+            headers: DOUBAN_API_HEADER,
+        });
+        return parseDoubanMovieInfo(response.data);
+    } catch (err) {
+        console.error("抓取失败:", err);
+        return {};
+    }
+}
+
+async function fetchDoubanVideoInfoFromApi(id: string): Promise<Record<string, any>>{
+    const URL_API = (id: string) => `https://m.douban.com/rexxar/api/v2/movie/${id}`;
+    try {
+        const response = await axios.get(URL_API(id), {
+            headers: DOUBAN_API_HEADER,
+        });
+        const data = response.data;
+
+        const durations = data.durations as string[];
+        const durationsResult = durations.map(dura => {
+            const match = dura.match(/\d+/);
+            return match ? parseInt(match[0], 10) : null;
+        });
+
+        return {
+            original_title: data.original_title,
+            release_year: parseInt(data.year),
+            intro: data.intro,
+            director: data.directors,
+            language: data.languages,
+            actors: data.actors,
+            duration: durationsResult,
+            production_country: data.countries,
+            aka: data.aka
+        }
+    } catch (err) {
+        console.error("抓取失败:", err);
+        return {};
+    }
+}
+
+/**
+ * 解析豆瓣 HTML
+ */
+function parseDoubanMovieInfo(html: string): Record<string, any> {
+    const $ = cheerio.load(html);
+
+    const movieInfo: Record<string, any> = {};
+
+    // 英文 key 的提取器
+    const extractors: Record<string, () => any> = {
+        // 编剧
+        screenwriter: () =>
+            $('#info span:contains("编剧") .attrs a')
+                .map((_, el) => $(el).text().trim())
+                .get(),
+        // 类型
+        genres: () =>
+            $('span[property="v:genre"]')
+                .map((_, el) => $(el).text().trim())
+                .get(),
+        // 上映日期
+        release_date: () =>
+            $('span[property="v:initialReleaseDate"]')
+                .map((_, el) => $(el).text().trim())
+                .get(),
+    };
+
+    // 基础字段（英文 key）
+    for (const key of Object.keys(extractors)) {
+        movieInfo[key] = extractors[key]();
+    }
+
+    // info 区域其他字段（IMDb）
+    $("#info .pl").each((_, el) => {
+        const label = $(el).text().trim().replace(/:$/, "");
+        const textNode = el.next;
+        const value =
+            textNode && textNode.type === "text" ? textNode.data.trim() : "";
+
+        switch (label) {
+            case "IMDb":
+                movieInfo.imdb = value;
+                break;
+            default:
+                break;
+        }
+    });
+
+    return movieInfo;
+}
+
+/**
+ *  根据豆瓣id过去视频详情信息
+ * @param id
+ */
+async function fetchDoubanVideoInfoById(id: string) : Promise<Record<string, any>>{
+    const [res1, res2] = await Promise.all([
+        fetchDoubanVideoInfo(id),
+        fetchDoubanVideoInfoFromApi(id),
+
+    ]);
+    // 后者的同名字段会覆盖前者
+    return {...res1, ...res2};
 }
